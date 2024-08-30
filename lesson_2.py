096922c6c1a9953b06f991eebb3a3988ad95d87b
Hernandez <ahernandez599@cps.k12.il.us>
2024-08-30 09:23:40 -0500

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,40 @@
+# comments are used to explain the code
+#this is a comment it will not be excused 
+#comments are notes to the programmer 
+#print functionallows you to print a message to the console
+print("day 3 python programming")
+# print function allows us to make an output to the console
+#input function allows  you to get input from the user
+#input function will return a string]
+name = input(" what is ur name")
+print ("Hello"+ name)
+#name is the variable that will store the input from the user
+#a variable is a container that stores data
+
+#ask the user for their age 
+# print out the age of the user with their name 
+#ask for their home address
+#print out the home adress of the user 
+#ask for their phone number 
+#print out the phone number of the user
+age = input("what is ur age?")
+print("Hello you are "+age+"years old")
+home_address = input ("what is ur home address?")
+print ("You live at"+ home_address)
+number = input("what is ur phone number?")
+print ("your phone number is "+ number)
+#with a variable using the plus sign
+#ask for their email address
+#print out email address
+Email= input("what is ur email address")
+print("Your email address is"+Email)
+#data types variables can store different types of data
+#strings are text data
+#integers are whole numbers
+# floats are decimal numbers
+# boolean are true or false values
+#Lists are a collection of data
+#strings are text data-- string data type
+name= "Alan" # whenever you see text in quotes it is a string
+age=15 # no quotes around the number
+is_student =True #True or False
